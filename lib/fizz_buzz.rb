58ff130b74c9e28c77ca6bc5062e0e07f7eb1283
Christer <christerforsgren91@gmail.com>
2021-03-15 13:00:08 +0100

    added a happy path specs for FizzBuzz .check method

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,16 @@
-def fizz_buzz(number)
-    number
+
+class FizzBuzz
+    def check(number)
+        if number % 15 == 0
+            'FizzBuzz'
+        elsif number % 5 == 0
+            'Buzz'
+        elsif number % 3 == 0
+            'Fizz'
+        else
+            number
+        end
+    end
 end
+
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,10 +1,20 @@
 require './lib/fizz_buzz'
 
-describe "fizz_buzz" do
-
-    it 'returns 1 if number is 1' do
-        expect(fizz_buzz(1)).to eq 1
-
+describe FizzBuzz do
+    subject { FizzBuzz.new }
+    it 'is expected to return a number if no game conditions are met' do 
+        expect(subject.check(1)).to eq 1
     end
 
+    it 'is expected to return fizz if number is divisable by 3' do
+        expect(subject.check(3)).to eq 'Fizz'
+    end
+
+    it 'is expected to return Buzz if number is divisable by 5' do
+        expect(subject.check(5)).to eq 'Buzz'
+    end
+
+    it 'is expected to return FizzBuzz if number is divisable by 15' do
+        expect(subject.check(15)).to eq 'FizzBuzz'
+    end
 end
